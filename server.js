7daaab4076d3498c80ffcf20d918e27655400e21
Hyejin <hj_yeo@naver.com>
2021-09-02 15:17:11 +0200

    Modification: Indentation Space 2 -> 4

--- server.js
+++ server.js
@@ -49,9 +49,9 @@ const server = http.createServer(app);
 // (Un écouteur d'évènements est également enregistré, consignant le port ou le canal nommé sur lequel le serveur s'exécute dans la console.)
 server.on('error', errorHandler);
 server.on('listening', () => {
-  const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-  console.log('Listening on ' + bind);
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+    console.log('Listening on ' + bind);
 });
 
 server.listen(port);
